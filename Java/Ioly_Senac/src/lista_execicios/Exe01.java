3a770a1e809fbedb0918f4a6982bf47d822689f7
Ioly Mayara Mesquita Oliveira <87779906+iolymmoliveira@users.noreply.github.com>
2023-08-04 20:37:05 -0300

    Java

---
+++ Java/Ioly_Senac/src/lista_execicios/Exe01.java
@@ -0,0 +1,31 @@
+package lista_execicios;
+
+public class Exe01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int intNum1, intNum2;
+		float fltNum1, fltNum2;
+		
+		intNum1 = 20/2;
+		System.out.println(intNum1);
+		System.out.println();
+		
+		intNum1 = 10/20;
+		System.out.println(intNum1);
+		System.out.println();
+		
+		fltNum1 = 10/20;
+		System.out.println(fltNum1);
+		System.out.println();
+		
+		fltNum2 = (float)10/20;
+		System.out.println(fltNum2);
+		System.out.println();
+		
+		fltNum1 = (float)10/20;
+		System.out.println(fltNum1);
+		System.out.println();
+	}
+}
---
+++ Java/Ioly_Senac/src/lista_execicios/Exe02.java
@@ -0,0 +1,25 @@
+package lista_execicios;
+
+public class Exe02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tipos de Dados em java:");
+		System.out.println();
+		
+		System.out.println("Byte - Mínimo: " + Byte.MIN_VALUE);
+		System.out.println("Byte - Mínimo: " + Byte.MAX_VALUE);
+		System.out.println();
+		
+		System.out.println("Integer - Mínimo: " + Integer.MIN_VALUE);
+		System.out.println("Integer - Mínimo: " + Integer.MAX_VALUE);
+		System.out.println();
+		
+		System.out.println("Float - Mínimo: " + Float.MIN_VALUE);
+		System.out.println("Float - Mínimo: " + Float.MAX_VALUE);
+		System.out.println();
+		System.out.println("------------------------------------");
+		System.out.println();
+	}
+
+}
