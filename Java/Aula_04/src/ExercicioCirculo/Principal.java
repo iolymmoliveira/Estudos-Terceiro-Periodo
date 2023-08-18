7451edbab2f7de1012baceb8ec8c897675ef26e4
Ioly Mayara Mesquita Oliveira <87779906+iolymmoliveira@users.noreply.github.com>
2023-08-18 20:37:43 -0300

    Atualizacao_aula_04

--- Java/Aula_04/src/ExercicioCirculo/Principal.java
+++ Java/Aula_04/src/ExercicioCirculo/Principal.java
@@ -12,12 +12,17 @@ public class Principal {
 		circulo02.y = 20;
 		circulo02.raio = 55;
 		
+		System.out.println("Exercício 3:");
+		System.out.println("------------");
 		System.out.println("x: " + circulo02.x);
 		System.out.println("y: " + circulo02.y);
 		System.out.println("raio: " + circulo02.raio);
+		System.out.println("------------");
 		System.out.println("");
 		
 		//Exercício 4
+		System.out.println("Exercício 4:");
+		System.out.println("------------");
 		circulo03.x = -15;
 		circulo03.y = 5;
 		circulo03.raio = 10;
@@ -25,9 +30,12 @@ public class Principal {
 		System.out.println("x: " + circulo03.x);
 		System.out.println("y: " + circulo03.y);
 		System.out.println("raio: " + circulo03.raio);
+		System.out.println("------------");
 		System.out.println("");
 		
 		//Exercício 5
+		System.out.println("Exercício 5:");
+		System.out.println("------------");
 		segundo_circulo.x = 2;
 		segundo_circulo.y = 45;
 		segundo_circulo.raio = 9;
@@ -35,8 +43,26 @@ public class Principal {
 		System.out.println("x: " + segundo_circulo.x);
 		System.out.println("y: " + segundo_circulo.y);
 		System.out.println("raio: " + segundo_circulo.raio);
+		System.out.println("------------");
 		System.out.println("");
 		
+		
+		//Exercício 8
+		System.out.println("Exercício 8:");
+		System.out.println("------------");
+		Circulo circ1 = new Circulo();
+		circ1.x = 3;
+		circ1.y = 3;
+		circ1.raio = 6;
+		circ1.cor = "Violet";
+		circ1.calcularArea();
+		System.out.println("8.1: " + circ1.toString()); 
+		
+		circ1.moverCirculo(4,4);
+		circ1.calcularArea();
+		System.out.println("8.2: " + circ1.toString()); 
+		System.out.println("------------");
+		System.out.println("");
 	}
 
 }
